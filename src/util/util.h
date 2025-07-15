c2f9b020473c7dd0a2a3ad4cc16e932c37c93f15
fish <innaias@harveyosity.org>
2025-07-14 19:13:49 -0700

    add switching between full debug and partial debug info

--- src/util/util.h
+++ src/util/util.h
@@ -84,6 +84,10 @@ inline std::unique_ptr<T, QObjDeleter<T>> unique_qobj(T *ptr)
 template<typename T>
 using qobj_unique_ptr = std::unique_ptr<T, QObjDeleter<T>>;
 
+#ifdef DEBUG
 #define debug() qDebug() << QString(__FILE__) % "@" % QString::number(__LINE__) % QString(":")
+#else
+#define debug() qDebug()
+#endif
 
 #endif // UTIL_H
